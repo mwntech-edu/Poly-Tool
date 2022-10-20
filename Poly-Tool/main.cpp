7b179c02228ce1da906299501499c2a296335189
Dahlia-sung <108792798+Dahlia-sung@users.noreply.github.com>
2022-10-20 14:55:33 +0900

    Revert "Revert "Merge branch 'main' of https://github.com/mwntech-edu/Poly-Tool""
    This reverts commit 18a169fa989b25e5198cb7e9f70a280a7980d169.

--- Poly-Tool/main.cpp
+++ Poly-Tool/main.cpp
@@ -1,44 +1,10 @@
-﻿#include <iostream>
+#include <iostream>
 using namespace std;
 
 int main() {
 	cout << "hello world!!" << endl;
 
-<<<<<<< HEAD
-	cout << "한솔 최고!! XD" << endl;
-=======
-	cout << "기상캐스터가 타는 차는 ? -> 기온차" << endl;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
->>>>>>> ce7f178c69020a3ba66823bc1fa923502ff0b68c
 	cout << "Park hee jung" << endl;
->>>>>>> 43e3328b2cbba9c900d803df15502193ea138b50
+
 	return 0;
 }
